895d295419fa2cd036c54f91af10a0f609a2216c
Brad <brad.whitecross@independentsolutions.com.au>
2015-02-27 15:03:35 +1000

    Initial Commit

---
+++ iGuessPortraits/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iGuessPortraits
+//
+//  Created by Brad on 27/02/2015.
+//  Copyright (c) 2015 iOSDevCamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
